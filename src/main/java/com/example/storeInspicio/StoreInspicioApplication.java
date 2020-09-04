c0ce411554fd825f3b1c48a43f78c90701e38d4b
Najse <34438787+Najse@users.noreply.github.com>
2020-09-04 13:22:43 +0200

    testing

--- src/main/java/com/example/storeInspicio/StoreInspicioApplication.java
+++ src/main/java/com/example/storeInspicio/StoreInspicioApplication.java
@@ -1,18 +1,17 @@
 package com.example.storeInspicio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
 public class StoreInspicioApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(StoreInspicioApplication.class, args);
-    }
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -21,6 +20,8 @@ public class StoreInspicioApplication {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/stores").allowedOrigins("https://storetrackerdeluxe.herokuapp.com");
                 registry.addMapping("/api/suggestion").allowedOrigins("https://storetrackerdeluxe.herokuapp.com");
+                registry.addMapping("/api/stores").allowedOrigins("http://localhost:5000");
+                registry.addMapping("/api/suggestion").allowedOrigins("http://localhost:5000");
             }
         };
     }
