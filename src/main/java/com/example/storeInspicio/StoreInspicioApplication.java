417992c6ef511aadec3b4b8b122e9ef0ffc5154a
Najse <34438787+Najse@users.noreply.github.com>
2020-09-04 13:24:38 +0200

    ...

--- src/main/java/com/example/storeInspicio/StoreInspicioApplication.java
+++ src/main/java/com/example/storeInspicio/StoreInspicioApplication.java
@@ -1,17 +1,18 @@
 package com.example.storeInspicio;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.core.env.Environment;
 
 
 @SpringBootApplication
 public class StoreInspicioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreInspicioApplication.class, args);
+    }
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
